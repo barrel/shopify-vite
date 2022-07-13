3c42577a1f3766f9277f71ea24786654fcc344f7
Scott Polhemus <scott.polhemus@barrelny.com>
2022-07-13 10:29:35 -0400

    Fix lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
+const path = require('path')
+
 module.exports = {
   root: true,
   extends: ['standard-with-typescript'],
   parserOptions: {
-    project: './tsconfig.json'
-  }
+    project: path.resolve(__dirname, './tsconfig.json')
+  },
+  ignorePatterns: [
+    'dist'
+  ]
 }
