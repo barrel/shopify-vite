b3e02e459e62c7a4f46f7c181e1387000ce9ed57
Scott Polhemus <scott.polhemus@barrelny.com>
2022-08-05 11:24:27 -0400

    Theme updates

--- themes/seed-theme/vite.config.js
+++ themes/seed-theme/vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig({
   },
   plugins: [
     // "Shopify" plugin enables entrypoint detection, smart generation of script and link tags, and support for Shopify CDN-hosted assets
-    shopify(),
+    shopify({
+      additionalEntrypoints: ['modules/**/*.js']
+    }),
     // "Shopify Modules" plugin enables "modules" folder pattern
     shopifyModules(),
     // "Shopify Theme Settings" plugin enables managing theme settings schema in individual files under "config/src/*.json"
