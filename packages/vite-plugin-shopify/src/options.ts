91566ce57e84e072e3c96a04c0d7cf3784c19460
Scott Polhemus <scott.polhemus@barrelny.com>
2022-08-05 10:15:14 -0400

    Add "additionalEntrypoints" option

--- packages/vite-plugin-shopify/src/options.ts
+++ packages/vite-plugin-shopify/src/options.ts
@@ -3,12 +3,14 @@ import path from 'path'
 export interface VitePluginShopifyOptions {
   themeRoot?: string
   entrypointsDir?: string
+  additionalEntrypoints?: string[]
   sourceCodeDir?: string
 }
 
 export interface ResolvedVitePluginShopifyOptions {
   themeRoot: string
   entrypointsDir: string
+  additionalEntrypoints: string[]
   sourceCodeDir: string
 }
 
@@ -18,10 +20,12 @@ export const resolveOptions = (
   const themeRoot = typeof options.themeRoot !== 'undefined' ? path.normalize(options.themeRoot) : './'
   const sourceCodeDir = typeof options.sourceCodeDir !== 'undefined' ? path.normalize(options.sourceCodeDir) : 'frontend'
   const entrypointsDir = typeof options.entrypointsDir !== 'undefined' ? path.normalize(options.entrypointsDir) : path.join(sourceCodeDir, 'entrypoints')
+  const additionalEntrypoints = typeof options.additionalEntrypoints !== 'undefined' ? options.additionalEntrypoints : []
 
   return {
     themeRoot,
     sourceCodeDir,
-    entrypointsDir
+    entrypointsDir,
+    additionalEntrypoints
   }
 }
