5b21857afcd880360cfa6567cee445de584cceb9
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-09-14 22:49:37 -0700

    Load module entries

--- themes/vite-shopify-example/vite.config.ts
+++ themes/vite-shopify-example/vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
+import shopifyModules from 'vite-plugin-shopify-modules'
 
 export default defineConfig({
   plugins: [
     shopify({
       additionalEntrypoints: [
         'frontend/foo.ts', // relative to sourceCodeDir
-        'resources/bar.ts' // relative to themeRoot
+        'resources/bar.ts', // relative to themeRoot
+        'modules/**/*.{ts,js}'
       ]
-    })
+    }),
+    shopifyModules()
   ],
   build: {
     emptyOutDir: true
