928dd4bb9c0ed8c85bdab05cbc9da5c03dca12e2
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-11-09 18:15:23 -0800

    Tests for configuring vite

--- packages/vite-plugin-shopify/test/index.test.ts
+++ packages/vite-plugin-shopify/test/index.test.ts
@@ -1,10 +1,23 @@
 import { describe, expect, it } from 'vitest'
 import path from 'path'
-import { build } from 'vite'
+import { build, UserConfig } from 'vite'
 import shopify from '../src/index'
 import fs from 'fs/promises'
 
 describe('vite-plugin-shopify', () => {
+  it('configures Vite', () => {
+    const plugin = shopify({
+      themeRoot: path.join(__dirname, '__fixtures__'),
+      sourceCodeDir: path.join(__dirname, '__fixtures__', 'frontend')
+    })[0]
+
+    const config = plugin.config({}, { command: 'build', mode: 'production' }) as UserConfig
+
+    expect(config.build.manifest).toBe(true)
+    expect(config.build.rollupOptions.input[0]).toMatch('theme.css')
+    expect(config.build.rollupOptions.input[1]).toMatch('theme.ts')
+  })
+
   it('builds out .liquid files', async () => {
     await build({
       logLevel: 'silent',
