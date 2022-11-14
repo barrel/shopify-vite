9c1d14e3064df6ac9a6c3e51ae3196d0b902f0ca
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-11-13 17:25:12 -0800

    Test that vite-plugin-shopify runs as expected

--- packages/vite-plugin-shopify/test/index.test.ts
+++ packages/vite-plugin-shopify/test/index.test.ts
@@ -1,36 +1,19 @@
 import { describe, expect, it } from 'vitest'
 import path from 'path'
-import { build, normalizePath } from 'vite'
+import { build } from 'vite'
 import shopify from '../src'
 import fs from 'fs/promises'
 
 describe('vite-plugin-shopify', () => {
-  it('configures Vite', () => {
-    const plugin = shopify({
-      themeRoot: path.join(__dirname, '__fixtures__'),
-      sourceCodeDir: path.join(__dirname, '__fixtures__', 'frontend')
-    })[0]
-
-    const config = plugin.config({}, { command: 'build', mode: 'production' })
-
-    expect(config.base).toEqual('./')
-    expect(config.publicDir).toEqual(false)
-    expect(config.build.outDir).toEqual(path.join(__dirname, '__fixtures__', 'assets'))
-    expect(config.build.assetsDir).toEqual('')
-    expect(config.build.manifest).toEqual(true)
-    expect(config.build.rollupOptions.input).toEqual([
-      normalizePath(path.resolve(path.join(__dirname, '__fixtures__', 'frontend', 'entrypoints', 'theme.css'))),
-      normalizePath(path.resolve(path.join(__dirname, '__fixtures__', 'frontend', 'entrypoints', 'theme.ts')))
-    ])
-  })
-
-  it('builds out .liquid files', async () => {
+  it('runs as expected', async () => {
     await build({
       logLevel: 'silent',
-      plugins: [shopify({
-        themeRoot: path.join(__dirname, '__fixtures__'),
-        sourceCodeDir: path.join(__dirname, '__fixtures__', 'frontend')
-      })]
+      plugins: [
+        shopify({
+          themeRoot: path.join(__dirname, '__fixtures__'),
+          sourceCodeDir: path.join(__dirname, '__fixtures__', 'frontend')
+        })
+      ]
     })
 
     const tagsHtml = await fs.readFile(path.join(__dirname, '__fixtures__', 'snippets', 'vite-tag.liquid'), { encoding: 'utf8' })
