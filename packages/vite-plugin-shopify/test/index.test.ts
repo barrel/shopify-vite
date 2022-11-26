cf6d6c72be19f5d31626f3b08d4f86527957290a
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-11-25 18:18:53 -0800

    Mock current date to make test pass

--- packages/vite-plugin-shopify/test/index.test.ts
+++ packages/vite-plugin-shopify/test/index.test.ts
@@ -1,11 +1,25 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi, beforeEach, afterEach } from 'vitest'
 import path from 'path'
 import { build } from 'vite'
 import shopify from '../src'
 import fs from 'fs/promises'
 
 describe('vite-plugin-shopify', () => {
+  beforeEach(() => {
+    // tell vitest we use mocked time
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    // restoring date after each test run
+    vi.restoreAllMocks()
+  })
+
   it('builds out .liquid files for production', async () => {
+    // mock current date
+    const mockDate = new Date(2022, 0, 1)
+    vi.setSystemTime(mockDate)
+
     await build({
       logLevel: 'silent',
       plugins: [
