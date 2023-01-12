fe61b2a95aefbd77a5da4d737663189b6b151366
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-01-11 20:05:37 -0800

    Move WIP docs outside the packages directory

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,5 +1,5 @@
 import 'nextra-theme-docs/style.css'
 
-export default function Nextra({ Component, pageProps }) {
+export default function Nextra ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
