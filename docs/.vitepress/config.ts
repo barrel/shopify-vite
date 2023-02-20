c65a405ab495d3a61a742f6e6ce173f5973608cc
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-02-19 22:27:45 -0800

    Add home page

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,15 @@
+export default {
+  title: 'Shopify Vite Plugin',
+  description: 'Vite integration for Shopify themes',
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]
+  ],
+
+  themeConfig: {
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright (c) Barrel LLC'
+    }
+  }
+}
