7154fa451fd47d246284a4e136e1ce6a478958fd
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-03-14 20:44:52 -0700

    Install Node deps

--- presets/theme/preset.ts
+++ presets/theme/preset.ts
@@ -4,6 +4,26 @@ export default definePreset({
     // ...
   },
   handler: async () => {
+    await group({
+      title: 'install Node dependencies',
+      handler: async () => {
+        await installPackages({
+          title: 'install front-end dependencies',
+          for: 'node',
+          install: [
+            'vite',
+            'vite-plugin-shopify'
+          ]
+        })
+
+        await editFiles({
+          files: 'package.json',
+          operations: [
+            { type: 'edit-json', merge: { scripts: { dev: 'vite', build: 'vite build' } } }
+          ]
+        })
+      }
+    })
     await extractTemplates()
     // ...
   }
