62db25a6ef87debd123640f73887abcbaa67aca3
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-03-15 17:59:26 -0700

    Refactor

--- presets/theme/preset.ts
+++ presets/theme/preset.ts
@@ -8,36 +8,61 @@ export default definePreset({
     `Edit your entry points in ${hl('frontend/entrypoints')}`,
     `Build for production with ${hl('npm run build')}`
   ],
-  handler: async () => {
-    await group({
-      title: 'install Node dependencies',
-      handler: async () => {
-        await installPackages({
-          title: 'install front-end dependencies',
-          for: 'node',
-          install: [
-            'vite',
-            'vite-plugin-shopify'
-          ]
-        })
-
-        await editFiles({
-          files: 'package.json',
-          operations: [
-            { type: 'edit-json', merge: { scripts: { dev: 'vite', build: 'vite build' } } }
-          ]
-        })
-      }
-    })
-
-    await group({
-      title: 'extract Vite scaffolding',
-      handler: async () => {
-        await extractTemplates({
-          title: 'extract templates',
-          from: 'default'
-        })
-      }
-    })
+  handler: async (context) => {
+    await installVite()
   }
 })
+
+async function installVite (): Promise<void> {
+  await group({
+    title: 'install Node dependencies',
+    handler: async () => {
+      await installPackages({
+        title: 'install front-end dependencies',
+        for: 'node',
+        install: [
+          'vite',
+          'vite-plugin-shopify'
+        ]
+      })
+
+      await editFiles({
+        files: 'package.json',
+        operations: [
+          { type: 'edit-json', merge: { scripts: { dev: 'vite', build: 'vite build' } } }
+        ]
+      })
+
+      await editFiles({
+        title: 'update .gitignore',
+        files: '.gitignore',
+        operations: [{ type: 'add-line', position: 'prepend', lines: 'node_modules' }]
+      })
+
+      await editFiles({
+        title: 'add vite-tag snippet',
+        files: 'layout/theme.liquid',
+        operations: [{
+          type: 'add-line',
+          match: /content_for_header/,
+          position: 'before',
+          lines: [
+            '{%- render \'vite-tag\' with \'theme.css\' -%}',
+            '{%- render \'vite-tag\' with \'theme.js\' -%}',
+            ''
+          ]
+        }]
+      })
+    }
+  })
+
+  await group({
+    title: 'extract Vite scaffolding',
+    handler: async () => {
+      await extractTemplates({
+        title: 'extract templates',
+        from: 'default'
+      })
+    }
+  })
+}
