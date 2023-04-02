f2e3108b29cc1c9df3332c4b6c7d654afc2aca7d
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-04-02 00:26:56 -0700

    Test the output of the react refresh script during dev

--- packages/vite-plugin-shopify/test/html.test.ts
+++ packages/vite-plugin-shopify/test/html.test.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 import fs from 'fs/promises'
 import html from '../src/html'
 import { resolveOptions } from '../src/options'
-import { createServer } from 'vite'
+import { createServer, Plugin } from 'vite'
 
 describe('vite-plugin-shopify:html', () => {
   it('builds out .liquid files for development', async () => {
@@ -18,6 +18,45 @@ describe('vite-plugin-shopify:html', () => {
       await createServer({
         logLevel: 'silent',
         configFile: path.join(__dirname, '__fixtures__', 'vite.config.js')
+      })
+    ).listen()
+
+    configureServer(viteServer)
+
+    viteServer.httpServer?.emit('listening')
+
+    const tagsHtml = await fs.readFile(path.join(__dirname, '__fixtures__', 'snippets', 'vite-tag.liquid'), { encoding: 'utf8' })
+
+    await viteServer.close()
+
+    expect(tagsHtml).toMatchSnapshot()
+  })
+
+  it('builds out .liquid files for development with the react refresh script', async () => {
+    const options = resolveOptions({
+      themeRoot: 'test/__fixtures__',
+      sourceCodeDir: 'test/__fixtures__/frontend'
+    })
+
+    const { configureServer } = html(options)
+
+    // Fakes for the vite-plugin-react plugin
+    const plugins: Plugin[] = [
+      // https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/src/index.ts#L104
+      {
+        name: 'vite:react-babel'
+      },
+      // https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/src/index.ts#L269
+      {
+        name: 'vite:react-refresh'
+      }
+    ]
+
+    const viteServer = await (
+      await createServer({
+        logLevel: 'silent',
+        configFile: path.join(__dirname, '__fixtures__', 'vite.config.js'),
+        plugins
       })
     ).listen()
 
