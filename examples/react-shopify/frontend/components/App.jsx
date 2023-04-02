f5f3dc6de8ef6cfbac9976aab28e0ed444cec047
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-04-01 22:14:52 -0700

    Add example

---
+++ examples/react-shopify/frontend/components/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ examples/react-shopify/frontend/entrypoints/theme.jsx
@@ -0,0 +1,10 @@
+import 'vite/modulepreload-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '@/components/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
