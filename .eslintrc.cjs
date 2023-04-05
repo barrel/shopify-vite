6e8a63f906c208ac458e1c66009975876becf5a7
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-04-04 20:33:54 -0700

    Update ESLint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.eslint.json',
+      './packages/*/tsconfig.json',
+      './examples/*/tsconfig.json',
+      './preset/tsconfig.json'
+    ]
+  }
+}
