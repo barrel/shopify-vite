b2aadf47cc70bd5454d02c15d3529b3b6607672f
Miguel Montalvo <miguel.montalvo@meetdomaine.com>
2025-03-26 23:54:49 -0600

    wip

--- examples/preact-shopify/vite.config.js
+++ examples/preact-shopify/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     shopify(),
-    preact()
+    preact(),
+    tailwindcss()
   ]
 })
