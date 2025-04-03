0d0efe98441d8c05794b75910be22a8640900ed7
Miguel Montalvo <miguel.montalvo@meetdomaine.com>
2025-04-03 14:55:09 -0600

    Use jsdelivr for @shopify/theme-hot-reload
    unpkg has issues

--- packages/vite-plugin-shopify/src/constants.ts
+++ packages/vite-plugin-shopify/src/constants.ts
@@ -13,6 +13,7 @@ export const CSS_EXTENSIONS_REGEX = new RegExp(
   `\\.(${KNOWN_CSS_EXTENSIONS.join('|')})(\\?.+)?$`
 )
 
-// https://github.com/Shopify/cli/blob/ef425e97dd46cc27ca1f71a5fd8dc7b190c99047/packages/theme/src/cli/utilities/theme-environment/hot-reload/server.ts#L357-L358
+// https://github.com/Shopify/cli/blob/ef425e97dd46cc27ca1f71a5fd8dc7b190c99047/packages/theme/src/cli/utilities/theme-environment/hot-reload/server.ts#L357
 export const hotReloadScriptId = 'hot-reload-client'
-export const hotReloadScriptUrl = 'https://unpkg.com/@shopify/theme-hot-reload'
+
+export const hotReloadScriptUrl = 'https://cdn.jsdelivr.net/npm/@shopify/theme-hot-reload/dist/theme-hot-reload.min.js'
